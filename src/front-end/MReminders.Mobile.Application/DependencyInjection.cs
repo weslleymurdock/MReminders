c3e819a71a545fedf88167b61fbcb716bbbbc961
Weslley Murdock <weslleyluiz@outlook.com.br>
2025-01-28 20:57:19 -0300

    Add application layer for mobile app (frontend)

---
+++ src/front-end/MReminders.Mobile.Application/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿namespace MReminders.Mobile.Application;
+
+public static class DependencyInjection
+{
+    public static MauiAppBuilder AddApplication(this MauiAppBuilder builder)
+    {
+        builder.Services.AddMediatR(x => x.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+        return builder;
+    }
+}
---
+++ src/front-end/MReminders.Mobile.Application/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace MReminders.Mobile.Application
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
