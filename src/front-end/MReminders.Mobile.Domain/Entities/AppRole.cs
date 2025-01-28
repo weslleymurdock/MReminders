46890b9104ed7e26af0cc2ee649d61180dcd4ed3
Weslley Murdock <weslleyluiz@outlook.com.br>
2025-01-28 20:56:22 -0300

    Add domain layer for mobile app (frontend)

---
+++ src/front-end/MReminders.Mobile.Domain/Entities/AppRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace MReminders.Mobile.Domain.Entities;
+[PrimaryKey(nameof(Id))]
+public class AppRole : IdentityRole<string>
+{
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+}
---
+++ src/front-end/MReminders.Mobile.Domain/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace MReminders.Mobile.Domain.Entities;
+
+[PrimaryKey(nameof(Id))]
+public class AppUser : IdentityUser<string>
+{
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+}
