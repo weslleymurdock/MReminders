5c2095c9cf7953114845d23f3ba69b648903f0b5
Weslley <weslleyluiz@outlook.com.br>
2025-01-28 22:25:06 -0300

    add backend tests

---
+++ tests/back-end/MReminders.Tests.E2ETests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace MReminders.Tests.E2ETests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/back-end/MReminders.Tests.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace MReminders.Tests.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
